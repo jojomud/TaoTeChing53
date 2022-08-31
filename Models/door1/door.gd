6fcc4351746a39d15db8001d7dd298bdcc3ef0a6
jojomud <68484154+jojomud@users.noreply.github.com>
2022-08-30 22:13:43 -0700

    door did stuff

--- Models/door1/door.gd
+++ Models/door1/door.gd
@@ -1,4 +1,4 @@
-extends Node
+extends KinematicBody
 
 
 # Declare member variables here. Examples:
@@ -14,3 +14,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area_body_entered(body):
+	$AnimationPlayer.play("Door close")
